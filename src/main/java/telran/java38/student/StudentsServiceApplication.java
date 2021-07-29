865b62e0a43b20f3428139b5b2fe6c3b421ed9f1
EddFish <edwardlfish@gmail.com>
2021-07-29 20:45:34 +0300

    First commit

---
+++ src/main/java/telran/java38/student/StudentsServiceApplication.java
@@ -0,0 +1,13 @@
+package telran.java38.student;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentsServiceApplication.class, args);
+	}
+
+}
